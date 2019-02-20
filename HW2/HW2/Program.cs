51b6242170816528f02c503f915a0427b8a21396
Viktor <v.mun@yahoo.com>
2019-02-20 22:19:00 +0100

    3task

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*3.	С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечётных положительных чисел.*/
 namespace HW2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int k = 0, s = 0;
+            Console.WriteLine("Введите числа:");
+            int a = int.Parse(Console.ReadLine());
+            while (a != 0)
+            {
+                if (a % 2 != 0&& a>0) { s += a; }
+                a = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Сумма нечетных чисел {s}");
+            Console.ReadKey();
+
         }
     }
 }
