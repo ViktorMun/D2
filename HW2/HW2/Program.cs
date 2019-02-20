3121c9a0f88009b5bfbef0bc03b31bc98bad86ba
Viktor <v.mun@yahoo.com>
2019-02-20 21:32:49 +0100

    2task

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*2.	Написать метод подсчета количества цифр числа.*/
 namespace HW2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            while (n != 0)
+            {
+                count++;
+                n = n / 10;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
