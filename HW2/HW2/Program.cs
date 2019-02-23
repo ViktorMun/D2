ec6e1ad23eb3cb8f0e5e5346357eb73cc9769d4d
Viktor <v.mun@yahoo.com>
2019-02-23 13:19:14 +0100

    Task-6

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -10,6 +10,31 @@ namespace HW2
     {
         static void Main(string[] args)
         {
+            DateTime time1 = DateTime.Now;
+            int total = 0;
+            for (int i = 1; i < 1000000000; i++)
+            {
+                if (goodNum(i)) total++;
+            }
+       
+
+            Console.WriteLine("Хорошие числа: " + total);
+            DateTime time2 = DateTime.Now;
+            Console.WriteLine("Время выполнения: {0}", (time2 - time1));
+            Console.ReadKey();
         }
+
+
+        static bool goodNum(int i)
+        {
+            int x = i;
+            int e = 0;
+            while (x > 0)
+            {
+                e += x % 10;
+                x /= 10;
+            }
+            return i % e == 0;
+                    }
     }
 }
